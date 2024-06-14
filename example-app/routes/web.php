ebd61832b3ce4c07188396206e9a98ec21b24537
AnnaSt <anutabatuk@gmail.com>
2024-06-14 12:13:09 +0800

    third

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -14,9 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect('/groups');
+    return view('/group');
 });
 
+Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
